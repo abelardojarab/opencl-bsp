4084f547c0510d1d4ff860202582aad843abafc6
Rauer, Christopher <christopher.rauer@intel.com>
2017-02-22 13:55:02 -0800

    got sim working with hw bsp

--- ase/run_sim.sh
+++ ase/run_sim.sh
@@ -1,24 +1,16 @@
 echo $KDIR
 mkdir qsys_files
-rm  qsys_files/*
+rm  -fr qsys_files/*
 
-cp $KDIR/sim_files/* ./qsys_files
+cp -R $KDIR/sim_files/* ./qsys_files
 rm qsys_files/*.vhd
 
 ###setup MPF with ASE
-ln -s $MPF_INSTALL_PATH mpf_src
+#ln -s $MPF_INSTALL_PATH mpf_src
 
 pkill -9 ase_simv
 
-(cat vlog_files_base.list ; find ./qsys_files ) > ./vlog_files.list
+(cat vlog_files_base.list ; find ./qsys_files | grep -v mpf ) > ./vlog_files.list
 make
 cp $KDIR/*.hex ./work/
 make sim&
-
-
-while [ ! -f ./work/inter.vpd]
-do
-  sleep 1
-done
-
-sleep 15
--- hardware/skx_fpga_dcp/sim_compile.sh
+++ hardware/skx_fpga_dcp/sim_compile.sh
@@ -1,4 +1,4 @@
-echo hello
+echo ase sim compile flow
 mkdir sim_files
 #cp -rf system_board_kernel_clk_generator.v ./system/simulation/submodules/system_board_kernel_clk_generator.v
 
@@ -20,13 +20,8 @@ find ./board/*/synth/*.v | xargs cp -t ./sim_files
 find ./board/*/synth/*.sv | xargs cp -t  ./sim_files
 find ./board/*/synth/*.vh  | xargs cp -t ./sim_files
 
-find ./ccip_async_v2/qsys/*/synth/*.sv | xargs cp -t  ./sim_files
-find ./ccip_async_v2/qsys/*/synth/*.v  | xargs cp -t ./sim_files
-find ./ccip_async_v2/qsys/*/*/synth/*.sv | xargs cp -t  ./sim_files
-find ./ccip_async_v2/qsys/*/*/synth/*.v  | xargs cp -t ./sim_files
 find ./ip/*.v | xargs cp -t ./sim_files
-find ./ip/*.sv| xargs cp -t  ./sim_files
-
+find ./ip/*.sv | xargs cp -t  ./sim_files
 
 cp -rf ccip_std_afu.sv ./sim_files/ccip_std_afu.sv
 find *.sv  | xargs cp -t ./sim_files
@@ -34,11 +29,11 @@ find *.sv  | xargs cp -t ./sim_files
 
 sed -i 's/RRP_FIFO_DEPTH(64)/RRP_FIFO_DEPTH(256)/g'  ./sim_files/*_system.v
 
-cp -rf global_routing.v ./sim_files/global_routing.v
+cp -rf extra_sim_files/global_routing.v ./sim_files/global_routing.v
 cp -rf system.v ./sim_files/system.v
 cp -rf rr_arb.v ./sim_files/rr_arb.v
 cp -rf bsp_logic.sv ./sim_files/bsp_logic.sv
-cp -rf ccip_async_v2/hw/ccip_async_shim.sv ./sim_files/ccip_async_shim.v
+cp -fr hw sim_files/mpf
 rm simulation.tar.gz
 tar -zcvf simulation.tar.gz sim_files sys_description.hex ccip_std_afu.v *.hex 
 cp -rf simulation.tar.gz fpga.bin
