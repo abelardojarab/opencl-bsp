e7aafb6ac39441361bca02a9a9b77eb7a9582365
Rauer, Christopher <christopher.rauer@intel.com>
2017-02-27 09:51:56 -0800

    updated farm compile script

--- example_designs/compile_all_examples_on_arc.sh
+++ example_designs/compile_all_examples_on_arc.sh
@@ -2,17 +2,21 @@
 #get exact script path
 SCRIPT_PATH=`readlink -f ${BASH_SOURCE[0]}`
 #get director of script path
-SCRIPT_DIR_PATH="$(dirname $SCRIPT_PATH)/../scripts/"
+SCRIPT_DIR_PATH="$(dirname $SCRIPT_PATH)"
+MAIN_SCRIPTS_DIR_PATH="$(dirname $SCRIPT_PATH)/../scripts/"
 
-#arc submit node/"[memory>=15000]" -- ../../../scripts/aoc_for_bsp.sh --board skx_fpga_1602 hello_world.cl 
-#arc submit node/"[memory>=15000]" -- ../../../scripts/aoc_for_bsp.sh --board skx_fpga_1602 device/mem_bandwidth.cl
-#arc submit node/"[memory>=15000]" -- ../../../scripts/aoc_for_bsp.sh --board skx_fpga_1602 device/vector_add.cl 
+AOC_CMD="sh $MAIN_SCRIPTS_DIR_PATH/aoc_for_bsp.sh --board skx_fpga_dcp"
 
-AOC_CMD="sh $SCRIPT_DIR_PATH/aoc_for_bsp.sh --board skx_fpga_1602"
+KERNEL_LIST=`find $SCRIPT_DIR_PATH -name "*.cl"`
 
 mkdir kernel_comp
 cd kernel_comp
-arc submit node/"[memory>=15000]" -- $AOC_CMD ../blank/blank.cl
-arc submit node/"[memory>=15000]" -- $AOC_CMD ../hello_world/device/hello_world.cl
-arc submit node/"[memory>=15000]" -- $AOC_CMD ../mem_bandwidth/device/mem_bandwidth.cl
-arc submit node/"[memory>=15000]" -- $AOC_CMD ../vector_add/device/vector_add.cl
+
+echo $KERNEL_LIST
+
+for i in $KERNEL_LIST; do
+	echo $i
+	arc submit node/"[memory>=15000]" priority=61 -- $AOC_CMD $i
+done
+exit 0
+
