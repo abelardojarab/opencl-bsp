7ec57359f5ab290b6b8f1d11d6bf7cb047063c98
Rauer, Christopher <christopher.rauer@intel.com>
2017-03-02 11:06:44 -0800

    fixed problem in sim script

--- scripts/run_complete_sim_test.sh
+++ scripts/run_complete_sim_test.sh
@@ -6,5 +6,6 @@ SCRIPT_DIR_PATH="$(dirname $SCRIPT_PATH)"
 
 ARC_RESOURCES_NEEDED="vcs,vcs-vcsmx-lic/vrtn-dev,gcc/4.8.2,acl/16.0.2,vcs,acds/16.0.2,qedition/pro,python"
 
+export OPENCL_ASE_SIM=1
 arc shell $ARC_RESOURCES_NEEDED -- $SCRIPT_DIR_PATH/setup_packages.sh
 arc shell $ARC_RESOURCES_NEEDED -- $SCRIPT_DIR_PATH/run_full_opencl_sim_test.sh
