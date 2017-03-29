db051f743a04bc84fea08a62a4c2f9a80f47bbed
Rauer, Christopher <christopher.rauer@intel.com>
2017-03-29 14:28:43 -0700

    fixed blue bit import script

--- hardware/skx_fpga_dcp_svm/import_blue_bits.sh
+++ hardware/skx_fpga_dcp_svm/import_blue_bits.sh
@@ -29,6 +29,7 @@ copy_platform_file() {
 rm -fr "output_files"
 mkdir "output_files"
 
+copy_platform_file "$FULL_PLATFORM_PATH/dcp.qsf" "."
 copy_platform_file "$FULL_PLATFORM_PATH/dcp.qdb" "."
 copy_platform_file "$FULL_PLATFORM_PATH/output_files/dcp.sof" "output_files"
 copy_platform_file "$FULL_PLATFORM_PATH/output_files/dcp.static.msf" "output_files"
@@ -38,7 +39,6 @@ copy_platform_file "$FULL_PLATFORM_PATH/output_files/dcp.sof" "output_files"
 copy_platform_file "$FULL_PLATFORM_PATH/../empty_afu/afu/interfaces/fpga_defines.vh"		"./afu/fpga_defines.vh"
 copy_platform_file "$FULL_PLATFORM_PATH/../empty_afu/afu/interfaces/vendor_defines.vh"      "./afu/interfaces/vendor_defines.vh"
 copy_platform_file "$FULL_PLATFORM_PATH/../empty_afu/afu/interfaces/ccip_if_pkg.sv"         "./afu/interfaces/ccip_if_pkg.sv"
-copy_platform_file "$FULL_PLATFORM_PATH/../empty_afu/afu/interfaces/dcp_top.sdc" "dcp_top.sdc"
 copy_platform_file "$FULL_PLATFORM_PATH/../empty_afu/afu/green_bs.sv" "./afu/green_bs.sv"
 
 rsync -rvua --delete "$FULL_PLATFORM_PATH/../empty_afu/design/" design
