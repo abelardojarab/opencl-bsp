7848dfbe25a9d4490777ff2915ca0a710f079e34
Rauer, Christopher <christopher.rauer@intel.com>
2017-04-29 21:36:25 -0700

    added svm support in comments

--- scripts/run_full_opencl_sim_test.sh
+++ scripts/run_full_opencl_sim_test.sh
@@ -21,9 +21,11 @@ export OPENCL_ASE_SIM=1
 $SCRIPT_DIR_PATH/setup_packages.sh
 cd $ROOT_PROJECT_PATH/example_designs/mem_bandwidth
 rm -fr bin/mem_bandwidth
-aoc device/mem_bandwidth.cl -o bin/mem_bandwidth.aocx
+aoc device/mem_bandwidth.cl --board skx_fpga_dcp_ddr -o bin/mem_bandwidth.aocx
+#aoc device/mem_bandwidth.cl --board skx_fpga_dcp_svm -o bin/mem_bandwidth.aocx
 rm -fr mem_bandwidth_comp
 mv bin/mem_bandwidth mem_bandwidth_comp
 aocl program acl0 bin/mem_bandwidth.aocx
 make
 ./bin/mem_bandwidth 1
+#ENABLE_DCP_OPENCL_SVM=1 ./bin/mem_bandwidth 1
