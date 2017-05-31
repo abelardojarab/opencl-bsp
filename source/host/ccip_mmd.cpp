6710e365431da4daaf2695f901710a9480dedac5
Southern, Gabriel <gabriel.southern@intel.com>
2017-05-31 13:04:04 -0700

    MMD refactoring and code cleanup
    Initial work on MMD refactoring using fpga-api.  Creates a
    CcipDevice class to encapsulate information about the device used
    for OpenCL operations.  This is similar to the PCIe reference
    design.
    
    Initial implementation only introduces class to encapusulate data,
    but still maintains a single instance.  Future updates will support
    multiple devices at the same time.
    
    Coding style follows fpga-api with use of tabs for indentation.
    May need to revisit this choice and ensure consistency with other
    BSP work.
    
    Updates in response to Chris' comments (note commits squashed
    to work with Gerrit)
    
    Minor fixes to code refactoring
    
    1. Fix merge conflict with fpga_api_port
    
    2. Adjust delays so that YIELD_DELAY is only 1us on HW
    
    3. Remove unused constants for MMIO addresses
    
    4. Make CcipDevice a final class to prevent subclassing
       and eliminate need for virtual destructor. Also
       set compile mode to C++11 to support final.
    
    Change-Id: I44ea310ba9b38683341dbd211c5e38196ec9da19

--- source/host/ccip_mmd.cpp
+++ source/host/ccip_mmd.cpp
@@ -30,91 +30,16 @@
 #include <stdint.h>
 #include <unistd.h>
 
-#include <uuid/uuid.h>
-#include <fpga/enum.h>
-#include <fpga/access.h>
-#include <fpga/common.h>
-
 #include "aocl_mmd.h"
-#include "pkg_editor.h"
+#include "ccip_mmd_device.h"
 
-using namespace std;
-
-#define AFU_ID "C000C966-0D82-4272-9AEF-FE5F84570612"
-
-//for DDR through MMIO
-#define MEM_WINDOW_CRTL 0xc800
-#define MEM_WINDOW_MEM 0x10000
-#define MEM_WINDOW_SPAN (64*1024)
-#define MEM_WINDOW_SPAN_MASK ((long)(MEM_WINDOW_SPAN-1))
-
-
-// TODO: refactor globals
-// Previous MMD used many global variables maintain
-// for initial port but eventually refactor 
-#define MMDHANDLE 1
-aocl_mmd_interrupt_handler_fn kernel_interrupt = NULL;
-void * kernel_interrupt_user_data;
-aocl_mmd_status_handler_fn event_update = NULL;
-void * event_update_user_data;
-
-
-fpga_handle afc_handle;
-
-//TODO: these delays should probobly be less on HW
-//#define MMIO_DELAY()  usleep(1000)
-#define MMIO_DELAY()
-#define YIELD_DELAY()  usleep(1000)
-#define OPENCL_SW_RESET_DELAY()  usleep(500*1000)
-#define AFU_RESET_DELAY()  usleep(2000*1000)
-
-#ifdef SIM
-//TODO: put sim specific stuff here
-#endif
-
-//debugging
-#if 1
-#define DEBUG_PRINT(...) printf(__VA_ARGS__)
-#else
-#define DEBUG_PRINT(...)
-#endif
-
-#if 1
-#define DCP_DEBUG_MEM(...) printf(__VA_ARGS__)
-#else
-#define DCP_DEBUG_MEM(...) 
-#endif
-
-// Define handle values for kernel, kernel_clk (pLL), and global memory
-typedef enum {
-  CCIP_DFH_RANGE = 0x0000,  
-  AOCL_IRQ_POLLING_BASE = 0x0100,  
-  QPI_ADDR_RANGE = 0x2000,  
-  DEBUG_ADDR_RANGE = 0x3000,  
-  AOCL_MMD_KERNEL = 0x4000,      /* Control interface into kernel interface */
-  AOCL_MMD_MEMORY = 0x100000,      /* Data interface to device memory */
-  AOCL_MMD_PLL = 0xb000,         /* Interface for reconfigurable PLL */
-  AOCL_MMD_PR_BASE_ID = 0xcf80,
-  AOCL_MMD_VERSION_ID = 0xcfc0
-} aocl_mmd_interface_t;
-
-#define KERNEL_SW_RESET_BASE (AOCL_MMD_KERNEL+0x30)
-
-//macros
-#define HW_UNLOCK ;
-#define HW_LOCK ;
-
-#define RESULT_INT(X) {*((int*)param_value) = X; if (param_size_ret) *param_size_ret=sizeof(int);}
-#define RESULT_STR(X) do { \
-    unsigned Xlen = strlen(X) + 1; \
-    memcpy((void*)param_value,X,(param_value_size <= Xlen) ? param_value_size : Xlen); \
-    if (param_size_ret) *param_size_ret=Xlen; \
-  } while(0)
+// TODO: create map or some other data structure that supports multiple devices
+// and replace all uses of ccip_dev_global with appropriate lookup function
+CcipDevice *ccip_dev_global;
 
 // static helper functions
 static bool check_for_svm_env();
 
-//mmd implementation
 AOCL_MMD_CALL void * aocl_mmd_shared_mem_alloc( int handle, size_t size, unsigned long long *device_ptr_out )
 {
 	printf("aocl_mmd_shared_mem_alloc is not implemented\n");
@@ -127,28 +52,23 @@ AOCL_MMD_CALL void aocl_mmd_shared_mem_free ( int handle, void* host_ptr, size_t
 	exit(1);
 }
 
-
-
-// Reprogram the device
 int AOCL_MMD_CALL aocl_mmd_reprogram(int handle, void *data, size_t data_size)
 {
 	printf("aocl_mmd_reprogram is not implemented\n");
 	exit(1);
 	return 0;
 }
-  
+
 int AOCL_MMD_CALL aocl_mmd_yield(int handle)
 {
-	int address = AOCL_IRQ_POLLING_BASE;
-	int irqval = 0;
-	static int last_irqval = -1;
-	static int count = 1;
+	uint32_t irqval = 0;
 	DEBUG_PRINT("* Called: aocl_mmd_yield\n");
 	YIELD_DELAY();
-	aocl_mmd_read(NULL, NULL, 4, &irqval, 0, address);
 
+	ccip_dev_global->read_block(NULL, AOCL_IRQ_POLLING_BASE, &irqval, 0, 4);
+	DEBUG_PRINT("irqval: %u\n", irqval);
 	if(irqval) {
-		kernel_interrupt( handle, kernel_interrupt_user_data );
+		ccip_dev_global->yield();
 	}
 
 	return 0;
@@ -156,9 +76,13 @@ int AOCL_MMD_CALL aocl_mmd_yield(int handle)
 
 static bool check_for_svm_env()
 {
+	//SVM not yet supported so no reason to check right now
+#if 1
+	return false;
+#else
 	static bool env_checked = false;
 	static bool svm_enabled = false;
-	
+
 	if(!env_checked)
 	{
 		if(getenv("ENABLE_DCP_OPENCL_SVM")){
@@ -166,86 +90,96 @@ static bool check_for_svm_env()
 		}
 		env_checked = true;
 	}
-	
+
 	return svm_enabled;
+#endif
 }
-  	  
+
+
+// Macros used for acol_mmd_get_offline_info and aocl_mmd_get_info
+#define RESULT_INT(X) {*((int*)param_value) = X; if (param_size_ret) *param_size_ret=sizeof(int);}
+#define RESULT_STR(X) do { \
+	unsigned Xlen = strlen(X) + 1; \
+	memcpy((void*)param_value,X,(param_value_size <= Xlen) ? param_value_size : Xlen); \
+	if (param_size_ret) *param_size_ret=Xlen; \
+} while(0)
+
+
 int aocl_mmd_get_offline_info(
-    aocl_mmd_offline_info_t requested_info_id,
-    size_t param_value_size,
-    void* param_value,
-    size_t* param_size_ret )
+		aocl_mmd_offline_info_t requested_info_id,
+		size_t param_value_size,
+		void* param_value,
+		size_t* param_size_ret )
 {
-  int mem_type_info = (int)AOCL_MMD_PHYSICAL_MEMORY;
-  if(check_for_svm_env())
-  	  mem_type_info = (int)AOCL_MMD_SVM_COARSE_GRAIN_BUFFER;
-	
-  switch(requested_info_id)
-  {
-    case AOCL_MMD_VERSION:              RESULT_STR("14.1"); break;
-    case AOCL_MMD_NUM_BOARDS:           RESULT_INT(1); break;
-    case AOCL_MMD_VENDOR_NAME:          RESULT_STR("Intel Corp"); break;
-    case AOCL_MMD_BOARD_NAMES:          RESULT_STR("acl0"); break;
-    case AOCL_MMD_VENDOR_ID:            RESULT_INT(0); break;
-    case AOCL_MMD_USES_YIELD:           RESULT_INT(1); break;
-    case AOCL_MMD_MEM_TYPES_SUPPORTED:  RESULT_INT(mem_type_info); break;
-  }
-  
-  return 0;
+	int mem_type_info = (int)AOCL_MMD_PHYSICAL_MEMORY;
+	if(check_for_svm_env())
+		mem_type_info = (int)AOCL_MMD_SVM_COARSE_GRAIN_BUFFER;
+
+	switch(requested_info_id)
+	{
+		case AOCL_MMD_VERSION:              RESULT_STR("14.1"); break;
+		case AOCL_MMD_NUM_BOARDS:           RESULT_INT(1); break;
+		case AOCL_MMD_VENDOR_NAME:          RESULT_STR("Intel Corp"); break;
+		case AOCL_MMD_BOARD_NAMES:          RESULT_STR("acl0"); break;
+		case AOCL_MMD_VENDOR_ID:            RESULT_INT(0); break;
+		case AOCL_MMD_USES_YIELD:           RESULT_INT(1); break;
+		case AOCL_MMD_MEM_TYPES_SUPPORTED:  RESULT_INT(mem_type_info); break;
+	}
+
+	return 0;
 }
 
 int aocl_mmd_get_info(
-    int handle,
-    aocl_mmd_info_t requested_info_id,
-    size_t param_value_size,
-    void* param_value,
-    size_t* param_size_ret )
+		int handle,
+		aocl_mmd_info_t requested_info_id,
+		size_t param_value_size,
+		void* param_value,
+		size_t* param_size_ret )
 {
-  HW_LOCK;
-  switch(requested_info_id)
-  {
-    case AOCL_MMD_BOARD_NAME:            RESULT_STR("SKX DCP FPGA OpenCL BSP"); break;
-    case AOCL_MMD_NUM_KERNEL_INTERFACES: RESULT_INT(1); break;
-    case AOCL_MMD_KERNEL_INTERFACES:     RESULT_INT(AOCL_MMD_KERNEL); break;
-    #ifdef SIM 
-    case AOCL_MMD_PLL_INTERFACES:        RESULT_INT(-1); break;
-    #else
-    case AOCL_MMD_PLL_INTERFACES:        RESULT_INT(-1); break;
-    #endif
-    case AOCL_MMD_MEMORY_INTERFACE:      RESULT_INT(AOCL_MMD_MEMORY); break;
-    case AOCL_MMD_PCIE_INFO:             RESULT_STR("N/A"); break;
-    case AOCL_MMD_BOARD_UNIQUE_ID:       RESULT_INT(0); break;
-    case AOCL_MMD_TEMPERATURE:
-      {
-      	//TODO: I think FME has temperature reading.  If available, we could
-      	//probobly use that
-        float *r;
-        int temp = 0;
-        r = (float*)param_value;
-        *r = (float)temp;
-        if (param_size_ret)
-          *param_size_ret = sizeof(float);
-        break;
-      }
-  }
-  HW_UNLOCK;
-  return 0;
+	DEBUG_PRINT("called aocl_mmd_get_info\n");
+	switch(requested_info_id)
+	{
+		case AOCL_MMD_BOARD_NAME:            RESULT_STR("SKX DCP FPGA OpenCL BSP"); break;
+		case AOCL_MMD_NUM_KERNEL_INTERFACES: RESULT_INT(1); break;
+		case AOCL_MMD_KERNEL_INTERFACES:
+						     RESULT_INT(AOCL_MMD_KERNEL); break;
+#ifdef SIM
+		case AOCL_MMD_PLL_INTERFACES:        RESULT_INT(-1); break;
+#else
+		case AOCL_MMD_PLL_INTERFACES:        RESULT_INT(-1); break;
+#endif
+		case AOCL_MMD_MEMORY_INTERFACE:
+						     RESULT_INT(AOCL_MMD_MEMORY); break;
+		case AOCL_MMD_PCIE_INFO:             RESULT_STR("N/A"); break;
+		case AOCL_MMD_BOARD_UNIQUE_ID:       RESULT_INT(0); break;
+		case AOCL_MMD_TEMPERATURE:
+						     {
+							     //TODO: I think FME has temperature reading.  If available, we could
+							     //probobly use that
+							     float *r;
+							     int temp = 0;
+							     r = (float*)param_value;
+							     *r = (float)temp;
+							     if (param_size_ret)
+								     *param_size_ret = sizeof(float);
+							     break;
+						     }
+	}
+	return 0;
 }
+
+#undef RESULT_INT
+#undef RESULT_STR
 
 int AOCL_MMD_CALL aocl_mmd_set_interrupt_handler( int handle, aocl_mmd_interrupt_handler_fn fn, void* user_data )
 {
-	int err;
-	kernel_interrupt = fn;
-	kernel_interrupt_user_data = user_data;
+	ccip_dev_global->set_kernel_interrupt(fn, user_data);
 	return 0;
 }
 
 int AOCL_MMD_CALL aocl_mmd_set_status_handler( int handle, aocl_mmd_status_handler_fn fn, void* user_data )
 {
-	// TODO: this code uses globals similar to Alex's old code.  The 
-	// A10 ref driver encapsulates device information in a class
-	event_update = fn;
-	event_update_user_data = user_data;
+	ccip_dev_global->set_status_handler(fn, user_data);
 	return 0;
 }
 
@@ -255,278 +189,55 @@ int AOCL_MMD_CALL aocl_mmd_write(
 		aocl_mmd_op_t op,
 		size_t len,
 		const void* src,
-		int mmd_interface, size_t offset )
+		int mmd_interface,
+		size_t offset)
 {
-	DCP_DEBUG_MEM("\n- aocl_mmd_write: %d\t %p\t %d\t %p\t %d\t %d\n",handle, op, len, src, mmd_interface, offset);
+	DCP_DEBUG_MEM("\n- aocl_mmd_write: %d\t %p\t %lu\t %p\t %d\t %lu\n",handle, op, len, src, mmd_interface, offset);
+	return ccip_dev_global->write_block(op, mmd_interface, src, offset, len);
 
-	fpga_result res = FPGA_OK;
-	if(mmd_interface == AOCL_MMD_MEMORY) {
-		DCP_DEBUG_MEM("DCP DEBUG: aocl_mmd_write called with AOCL_MMD_MEMORY!\n");
-		DCP_DEBUG_MEM("DCP DEBUG: len=%d offset = %08x, data = %08x\n", len, (unsigned)offset,((int *)src)[0]);
-
-		void * host_addr = const_cast<void *>(src);
-		long dev_addr  = offset;
-
-		long cur_mem_page = dev_addr & ~MEM_WINDOW_SPAN_MASK;
-		res = fpgaWriteMMIO64(afc_handle, 0, MEM_WINDOW_CRTL, cur_mem_page);
-		if(res != FPGA_OK) {
-			fprintf(stderr, "Error: aocl_mmd_write: %d\n", res);
-			exit(-1);
-		}
-		DCP_DEBUG_MEM("DCP DEBUG: set page %08x\n", cur_mem_page);
-		for(long i = 0; i < len/8; i++) {
-			long mem_page = dev_addr & ~MEM_WINDOW_SPAN_MASK;
-			if(mem_page != cur_mem_page) {
-				cur_mem_page = mem_page;
-				res = fpgaWriteMMIO64(afc_handle, 0, MEM_WINDOW_CRTL, cur_mem_page);
-				if(res != FPGA_OK) {
-					fprintf(stderr, "Error: aocl_mmd_write: %d\n", res);
-					exit(-1);
-				}	
-			DCP_DEBUG_MEM("DCP DEBUG: set page %08x\n", cur_mem_page);
-			}
-			res = fpgaWriteMMIO64(afc_handle, 0, MEM_WINDOW_MEM+(dev_addr&MEM_WINDOW_SPAN_MASK), ((long *)host_addr)[0]);
-			DCP_DEBUG_MEM("DCP DEBUG: write data %08x %08x %016lx\n", host_addr, dev_addr, ((long *)host_addr)[0]);
-
-			host_addr += 8;
-			dev_addr += 8;
-		}
-
-		DCP_DEBUG_MEM("DCP DEBUG: aocl_mmd_write done!\n");
-	} else {
-
-		unsigned long int address = mmd_interface + offset; // We defined it this way
-		
-		//HACK: need extra delay for opencl sw reset
-		if(address == KERNEL_SW_RESET_BASE)
-			OPENCL_SW_RESET_DELAY();
-
-  		MMIO_DELAY();
-		DEBUG_PRINT("mmd write: len: %d address = %09x, offset = %08x, data = %08x\n",len, address, (unsigned)offset,((int *)src)[0]);
-	
-		//TODO: add more robust bounds and type checking
-		uint64_t *src_addr64 = src;
-		while(len >= 8) {
-			res = fpgaWriteMMIO64(afc_handle, 0, address, *src_addr64);
-			if(res != FPGA_OK) {
-				fprintf(stderr,"Error MMIO read: %d\n",res);
-				exit(-1);
-			}
-			src_addr64 += 1;
-			address += 8;
-			len -= 8;
-		}
-		uint32_t *src_addr32 = reinterpret_cast<uint32_t *>(src_addr64);
-		while(len >= 4) {
-			res = fpgaWriteMMIO32(afc_handle, 0, address, *src_addr32);
-			if(res != FPGA_OK) {
-				fprintf(stderr,"Error MMIO read: %d\n",res);
-				exit(-1);
-			}
-			src_addr32 += 1;
-			address += 4;
-			len -= 4;
-		}
-		if(len > 0) {
-			//TODO: Potentially unsafe - rewrite to not overflow bounds
-			DEBUG_PRINT("Warning unaligned write\n");
-			res = fpgaWriteMMIO32(afc_handle, 0, address, *src_addr32);
-		}
-	}
-	if (op) {
-		//assert(event_update);
-		event_update(handle, event_update_user_data, op, 0);
-	}
-	//HW_UNLOCK;
-	return 0;
 }
 
 int AOCL_MMD_CALL aocl_mmd_read(
-    int handle,
-    aocl_mmd_op_t op,
-    size_t len,
-    void* dst,
-    int mmd_interface, size_t offset )
+		int handle,
+		aocl_mmd_op_t op,
+		size_t len,
+		void* dst,
+		int mmd_interface,
+		size_t offset)
 {
-	fpga_result res = FPGA_OK;
-
-	DCP_DEBUG_MEM("\n+ aocl_mmd_read: %d\t %p\t %d\t %p\t %d\t %d\n",handle, op, len, dst, mmd_interface, offset);
-	if(mmd_interface == AOCL_MMD_MEMORY) {
-		DCP_DEBUG_MEM("DCP DEBUG: aocl_mmd_read called with AOCL_MMD_MEMORY!\n");
-		DCP_DEBUG_MEM("DCP DEBUG: len: %d offset: %08x\n", len, offset);
-
-		void * host_addr = const_cast<void *>(dst);
-		long dev_addr  = offset;
-
-		long cur_mem_page = dev_addr & ~MEM_WINDOW_SPAN_MASK;
-		res = fpgaWriteMMIO64(afc_handle, 0, MEM_WINDOW_CRTL, cur_mem_page);
-		if(res != FPGA_OK) {
-			fprintf(stderr,"Error MMIO write: %d\n",res);
-			exit(-1);
-		}
-		DCP_DEBUG_MEM("DCP DEBUG: set page %08x\n", cur_mem_page);
-		for(long i = 0; i < len/8; i++)
-		{
-			long mem_page = dev_addr & ~MEM_WINDOW_SPAN_MASK;
-			if(mem_page != cur_mem_page)
-			{
-				cur_mem_page = mem_page;
-				res = fpgaWriteMMIO64(afc_handle, 0, MEM_WINDOW_CRTL, cur_mem_page);
-				if(res != FPGA_OK) {
-					fprintf(stderr,"Error MMIO write: %d\n",res);
-					exit(-1);
-				}
-				DCP_DEBUG_MEM("DCP DEBUG: set page %08x\n", cur_mem_page);
-			}
-			res = fpgaReadMMIO64(afc_handle, 0,MEM_WINDOW_MEM+(dev_addr&MEM_WINDOW_SPAN_MASK), host_addr);
-			if(res != FPGA_OK) {
-				fprintf(stderr,"Error MMIO write: %d\n",res);
-				exit(-1);
-			}
-			DCP_DEBUG_MEM("DCP DEBUG: read data %08x %08x %016lx\n", host_addr, dev_addr, ((long *)host_addr)[0]);
-
-			host_addr += 8;
-			dev_addr += 8;
-		}
-		DCP_DEBUG_MEM("DCP DEBUG: aocl_mmd_read done!\n");
-	} else {
-		int address = mmd_interface + offset; // We defined it this way
-
-		//HACK: need extra delay for opencl sw reset
-		if(address == KERNEL_SW_RESET_BASE)
-			OPENCL_SW_RESET_DELAY();
-		
-  		MMIO_DELAY();
-		DEBUG_PRINT("aocl_mmd_read len: %d offset: %d mmd_interface: %d   address: %d \n", len, offset, mmd_interface, address);
-
-		//TODO: add more robust bounds and type checking
-		uint64_t *dst_addr64 = dst;
-		while(len >= 8) {
-			res = fpgaReadMMIO64(afc_handle, 0, address, dst_addr64);
-			if(res != FPGA_OK) {
-				fprintf(stderr,"Error MMIO read: %d\n",res);
-				exit(-1);
-			}
-			dst_addr64 += 1;
-			address += 8;
-			len -= 8;
-		}
-		uint32_t *dst_addr32 = reinterpret_cast<uint32_t *>(dst_addr64);
-		while(len >= 4) {
-			res = fpgaReadMMIO32(afc_handle, 0, address, dst_addr32);
-				
-			if(res != FPGA_OK) {
-				fprintf(stderr,"Error MMIO read: %d\n",res);
-				exit(-1);
-			}
-			dst_addr32 += 1;
-			address += 4;
-			len -= 4;
-		}
-		if(len > 0) {
-			//TODO: Potentially unsafe - rewrite to not overflow bounds
-			DEBUG_PRINT("WARNING: unaligned read\n");
-			//res = fpgaReadMMIO32(afc_handle, 0, address, dst_addr32);
-			uint32_t read_data;
-			res = fpgaReadMMIO32(afc_handle, 0, address, &read_data);
-			memcpy(dst_addr32, &read_data, len);
-		}
-	}  
-	if (op)
-	{
-		//assert(event_update);
-		event_update(handle, event_update_user_data, op, 0);
-	}
-
-	//HW_UNLOCK;
-	return 0;
+	DCP_DEBUG_MEM("\n+ aocl_mmd_read: %d\t %p\t %lu\t %p\t %d\t %lu\n",handle, op, len, dst, mmd_interface, offset);
+	return ccip_dev_global->read_block(op, mmd_interface, dst, offset, len);
 }
 
 int AOCL_MMD_CALL aocl_mmd_copy(
-    int handle,
-    aocl_mmd_op_t op,
-    size_t len,
-    int mmd_interface, size_t src_offset, size_t dst_offset )
+		int handle,
+		aocl_mmd_op_t op,
+		size_t len,
+		int mmd_interface, size_t src_offset, size_t dst_offset )
 {
-	HW_LOCK;
 	printf("aocl_mmd_copy is not implemented\n");
 	exit(1);
-	HW_UNLOCK;
 	return 0;
 }
 
 int AOCL_MMD_CALL aocl_mmd_open(const char *name)
 {
-	fpga_properties    filter = NULL;
-	fpga_guid          guid;
+	DEBUG_PRINT("Opening device: %s\n", name);
 
-	fpga_result     res = FPGA_OK;
+	int unique_id = 1; // TODO: generate an acutal unique ID
+	int dev_num   = 1; // TODO: parse name to determine actual dev num
+	CcipDevice *ccip_dev = new CcipDevice(dev_num, unique_id);
 
-	if (uuid_parse(AFU_ID, guid) < 0) {
-		fprintf(stderr, "Error parsing guid '%s'\n", AFU_ID);
-		return -1;
+	if(ccip_dev->is_initialized()) {
+		ccip_dev_global = ccip_dev;
+	} else {
+		delete ccip_dev;
 	}
 
-	/* Look for AFC with AFU_ID */
-	res = fpgaGetProperties(NULL, &filter);
-	if(res != FPGA_OK) {
-		fprintf(stderr, "Error creating properties object\n");
-		return -1;
-	}
-	
-	res = fpgaPropertiesSetObjectType(filter, FPGA_AFC);
-	if(res != FPGA_OK) {
-		fprintf(stderr, "Error setting object type\n");
-		return -1;
-	}
-
-	res = fpgaPropertiesSetGuid(filter, guid);
-	if(res != FPGA_OK) {
-		fprintf(stderr, "Error setting GUID\n");
-		return -1;
-	}
-
-	fpga_token         afc_token;
-	uint32_t           num_matches;
-	
-	//TODO: Add selection via BDF / device ID
-	res = fpgaEnumerate(&filter, 1, &afc_token, 1, &num_matches);
-	if(res != FPGA_OK) {
-		fprintf(stderr, "Error enumerating AFCs: %d\n", res);
-		return -1;
-	}
-
-	if(num_matches < 1) {
-		fprintf(stderr, "AFC not found\n");
-		res = fpgaDestroyProperties(&filter);
-		return -1;
-	}
-
-	res = fpgaOpen(afc_token, &afc_handle, 0);
-	if(res != FPGA_OK) {
-		fprintf(stderr, "Error opening AFC: %d\n", res);
-		return -1;
-	}
-
-
-	res = fpgaMapMMIO(afc_handle, 0, NULL);
-	if(res != FPGA_OK) {
-		fprintf(stderr, "Error mapping MMIO space: %d\n", res);
-		return -1;
-	}
-	
-	/* Reset AFC */
-	res = fpgaReset(afc_handle);
-	if(res != FPGA_OK) {
-		fprintf(stderr, "Error resetting AFC: %d\n", res);
-		return -1;
-	}
-	AFU_RESET_DELAY();
-	
-	return MMDHANDLE;   //TODO: need to support multiple cards.  Keep track of handle
+	return 1;   // TODO: return an actual unique ID for handle
 }
 
-int AOCL_MMD_CALL  aocl_mmd_close(int handle) 
+int AOCL_MMD_CALL  aocl_mmd_close(int handle)
 {
 	printf("aocl_mmd_close is not implemented\n");
 	exit(1);
---
+++ source/host/ccip_mmd_device.h
@@ -0,0 +1,109 @@
+#ifndef _CCIP_MMD_DEVICE_H
+#define _CCIP_MMD_DEVICE_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#include <uuid/uuid.h>
+#include <fpga/enum.h>
+#include <fpga/access.h>
+#include <fpga/common.h>
+
+#include "pkg_editor.h"
+#include "aocl_mmd.h"
+
+// Tune delay for simulation or HW. Eventually delay
+// should be removed for HW, may still be needed for ASE simulation
+#ifdef SIM
+#define DELAY_MULTIPLIER 100
+#else
+#define DELAY_MULTIPLIER 1
+#endif
+
+// Delay settings
+// TODO: Figure out why these delays are needed and
+// have requirement removed (at least for HW)
+#define MMIO_DELAY()
+#define YIELD_DELAY()		usleep(1 * DELAY_MULTIPLIER)
+#define OPENCL_SW_RESET_DELAY()	usleep(5000 * DELAY_MULTIPLIER)
+#define AFU_RESET_DELAY()	usleep(20000 * DELAY_MULTIPLIER)
+
+#define KERNEL_SW_RESET_BASE (AOCL_MMD_KERNEL+0x30)
+
+//AFU IDs:
+#define MCP_OPENCL_AFU_ID "C000C966-0D82-4272-9AEF-FE5F84570612"
+#define DCP_OPENCL_SVM_AFU_ID "3A00972E-7AAC-41DE-BBD1-3901124E8CDA"
+#define DCP_OPENCL_DDR_AFU_ID "18B79FFA-2EE5-4AA0-96EF-4230DAFACB5F"
+
+
+//debugging
+#ifdef DEBUG
+#define DEBUG_PRINT(...) fprintf(stderr,__VA_ARGS__)
+#else
+#define DEBUG_PRINT(...)
+#endif
+
+#ifdef DEBUG_MEM
+#define DCP_DEBUG_MEM(...) fprintf(stderr,__VA_ARGS__)
+#else
+#define DCP_DEBUG_MEM(...)
+#endif
+
+#ifdef SIM
+//TODO: put sim specific stuff here
+#endif
+
+enum {
+	AOCL_IRQ_POLLING_BASE = 0x0100,
+	AOCL_MMD_KERNEL = 0x4000,	/* Control interface into kernel interface */
+	AOCL_MMD_MEMORY = 0x100000	/* Data interface to device memory */
+};
+
+class CcipDevice final
+{
+	public:
+	CcipDevice(int dev_num, int unique_id);
+	~CcipDevice();
+
+	bool is_initialized() { return initialized; }
+	fpga_handle get_handle() { return afc_handle; }
+
+	void set_kernel_interrupt(aocl_mmd_interrupt_handler_fn fn, void* user_data);
+	void set_status_handler(aocl_mmd_status_handler_fn fn, void* user_data);
+	int yield();
+	void event_update_fn(aocl_mmd_op_t op, int status);
+
+	int read_block(aocl_mmd_op_t op,
+			int mmd_interface,
+			void *host_addr,
+			size_t dev_addr,
+			size_t size);
+
+	int write_block(aocl_mmd_op_t op,
+			int mmd_interface,
+			const void *host_addr,
+			size_t dev_addr,
+			size_t size);
+
+	private:
+	aocl_mmd_interrupt_handler_fn kernel_interrupt;
+	void *kernel_interrupt_user_data;
+	aocl_mmd_status_handler_fn event_update;
+	void *event_update_user_data;
+
+	bool initialized;
+	fpga_handle       afc_handle;
+	fpga_properties   filter;
+	fpga_token        afc_token;
+
+	// Helper functions
+	int read_memory(uint64_t *host_addr, size_t dev_addr, size_t size);
+	int write_memory(const uint64_t *host_addr, size_t dev_addr, size_t size);
+	int read_mmio(void *host_addr, size_t dev_addr, size_t size);
+	int write_mmio(const void *host_addr, size_t dev_addr, size_t size);
+};
+
+#endif // _CCIP_MMD_DEVICE_H
