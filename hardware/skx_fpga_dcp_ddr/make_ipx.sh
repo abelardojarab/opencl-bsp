b74b8f0d53673f2d4d74c870b1664ee2f47c877f
Rauer, Christopher <christopher.rauer@intel.com>
2017-05-31 15:01:41 -0700

    initial qsys connections for new ccip interface

--- hardware/skx_fpga_dcp_ddr/make_ipx.sh
+++ hardware/skx_fpga_dcp_ddr/make_ipx.sh
@@ -1,2 +1,2 @@
 #ip-make-ipx --source-directory="iface,iface/ip" --output=ccip.ipx
-ip-make-ipx --source-directory="$ALTERAOCLSDKROOT/ip/board" --output=iface.ipx  --relative-vars=ALTERAOCLSDKROOT
+ip-make-ipx --source-directory="$ALTERAOCLSDKROOT/ip/board,ccip_iface,ccip_iface/avst_to_avmm_master,ccip_iface/avst_to_avmm_slave,msgdma_bbb" --output=iface.ipx  --relative-vars=ALTERAOCLSDKROOT
