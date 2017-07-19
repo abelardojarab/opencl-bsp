3436758a827d2b29ee1913cb00d95f1fb2c995f7
Rauer, Christopher <christopher.rauer@intel.com>
2017-07-19 13:20:25 -0700

    added inspur compile script

---
+++ example_designs/compile_all_examples_on_arc_inspur.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#get exact script path
+SCRIPT_PATH=`readlink -f ${BASH_SOURCE[0]}`
+#get director of script path
+SCRIPT_DIR_PATH="$(dirname $SCRIPT_PATH)"
+MAIN_SCRIPTS_DIR_PATH="$(dirname $SCRIPT_PATH)/../scripts/"
+
+AOC_CMD="sh $MAIN_SCRIPTS_DIR_PATH/aoc_for_bsp.sh --board dcp_inspur"
+
+KERNEL_LIST=`find $SCRIPT_DIR_PATH -name "*.cl"`
+
+mkdir kernel_comp_inspur
+cd kernel_comp_inspur
+
+echo $KERNEL_LIST
+
+for i in $KERNEL_LIST; do
+	echo $i
+	arc submit node/"[memory>=15000]" priority=61 -- $AOC_CMD $i
+done
+exit 0
+
