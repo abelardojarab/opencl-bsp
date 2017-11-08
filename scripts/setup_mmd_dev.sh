e46c3e41f39a2a048d3635b154572c6171496ea1
Southern, Gabriel <gabriel.southern@intel.com>
2017-11-07 21:53:02 -0800

    Add script to easily build MMD in ARC
    This script is used for setting up environment to build MMD
    in ARC.  I would have submitted as a separate PR, but since
    this PR is taking awhile to be reviewed I keep having to pull
    the script in so I'm submitting it so will be on the same branch
    and is easy to use while working on the MMD to fix things
    needed for multidevice support.

---
+++ scripts/setup_mmd_dev.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ORG_SCRIPT_PATH=${BASH_SOURCE[0]}
+SCRIPT_PATH=$(readlink -f $ORG_SCRIPT_PATH)
+SCRIPT_DIR_PATH="$(dirname $SCRIPT_PATH)"
+source ${SCRIPT_DIR_PATH}/bsp_common.sh
+
+if [ "$ARC_SW_CONFIGURED" !=  "1" ]; then
+	export ARC_SW_CONFIGURED=1
+	arc shell ${SW_BUILD_ARC_RESOURCES} -- ${ORG_SCRIPT_PATH} $@
+	unset ARC_SW_CONFIGURED
+fi
+
+if [ "$ARC_SW_CONFIGURED" == 1 ]; then
+	if [ ! -d "${OPAE_BUILD_PATH}" ]; then
+		OPAE_USE_GIT_ARCHIVE='0' ${SCRIPT_DIR_PATH}/build_opae.sh
+	else
+		pushd $PWD
+		cd $OPAE_SRC_PATH && git pull
+		cd $OPAE_BUILD_PATH && make install
+		popd
+	fi
+else
+	exec arc shell $ACDS_ARC_RESOURCES,$SW_BUILD_ARC_RESOURCES -- $@
+fi
