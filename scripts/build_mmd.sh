587ce81b99b45d9023f364b8d97f3621c7257b66
Southern, Gabriel <gabriel.southern@intel.com>
2017-11-15 00:13:09 -0800

    Use cmake when building mmd
    Switches build_mmd script to use cmake files introduced
    in previous commit.

--- scripts/build_mmd.sh
+++ scripts/build_mmd.sh
@@ -8,9 +8,15 @@ SCRIPT_DIR_PATH="$(dirname $SCRIPT_PATH)"
 #exit early and return error code if there is a problem
 set -e
 
+SET_ASE="OFF"
+if [ "$OPENCL_ASE_SIM" == "1" ]; then
+   SET_ASE="ON"
+fi
+
 cd $ROOT_PROJECT_PATH/source
-make clean
-make
+mkdir build && cd build
+CC=`which gcc` CXX=`which g++` cmake -DOPENCL_ASE_SIM=$SET_ASE -DCMAKE_INSTALL_PREFIX=$ROOT_PROJECT_PATH/linux64 $ROOT_PROJECT_PATH/source
+make && make install
 
 if [ "$OPENCL_ASE_SIM" == "1" ]; then
 	cp -f $AOCL_BOARD_PACKAGE_ROOT/ase/scripts/program $AOCL_BOARD_PACKAGE_ROOT/linux64/libexec/program
