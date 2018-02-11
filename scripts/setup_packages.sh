8ed7e8a0413c7144cb77f55aceb987245ae73917
Rauer, Christopher <christopher.rauer@intel.com>
2018-02-10 21:13:00 -0800

    removed old mpf sources

--- scripts/setup_packages.sh
+++ scripts/setup_packages.sh
@@ -14,13 +14,6 @@ if [ ! -d "$OPAE_INSTALL_PATH" ]; then
   sh $SCRIPT_DIR_PATH/build_opae.sh
 fi
 
-#TODO: reenable MPF build
-#check for MPF
-#if [ ! -d "$MPF_INSTALL_PATH" ]; then
-#  echo "MPF does not exist.  need to build"
-#  sh $SCRIPT_DIR_PATH/setup_mpf.sh
-#fi
-
 #check for MMD
 if [ ! -f "$AOCL_BOARD_PACKAGE_ROOT/linux64/lib/libintel_opae_mmd.so" ]; then
   echo "MMD does not exist.  need to build"
